86016f84956a6bd955733f230fb11af7eed7e4ef
kiku-991 <goseihou@gmail.com>
2021-04-30 19:45:31 +0900

    新規プロジェクト

---
+++ cecily/src/main/java/com/shoping/kiku/CecilyApplication.java
@@ -0,0 +1,13 @@
+package com.shoping.kiku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CecilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CecilyApplication.class, args);
+	}
+
+}
---
+++ cecily/src/test/java/com/shoping/kiku/CecilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoping.kiku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CecilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
